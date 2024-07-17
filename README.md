520e2e99c0e58ee903900e2e66ca5e34ecd153df
Mehmed <mehmed_duhovic@hotmail.com>
2024-07-17 23:49:51 +0200

    removing intermediate file and modify README and script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Convert To Markdown
+
+## Description
+A simple project that converts an html to a specific version of a markdown.
+
+## Installation
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the required packages.
+```bash
+pip install -r requirements.txt
--- script.py
+++ script.py
@@ -18,11 +18,7 @@ def read_content_from_filename(input_filename: str) -> str:
 
     filtered_lines = [line for line in lines if not line.strip().startswith('<!--')]
 
-    with open(intermediate_filename, 'w', encoding='utf-8') as file:
-        file.writelines(filtered_lines)
-
-    with open(intermediate_filename, 'r', encoding='utf-8') as file:
-        return file.read()
+    return filtered_lines
        
 def save_markdown(markdown_content: str, filename: str) -> None:
     with open(filename, 'w') as file:
@@ -37,6 +33,5 @@ def convert_txt_file_to_markdown(input_filename: str, output_filename: str) -> N
 
 if __name__ == '__main__':
     input_filename='markdown.txt'
-    intermediate_filename='intermediate.txt'
     output_filename='output.md'
     convert_txt_file_to_markdown(input_filename, output_filename)
